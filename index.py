f85372d8f3c8117d7ee15d0824cccd4e11ce95da
elcruzo <ayomideadekoya266@gmail.com>
2023-08-08 15:18:42 -0400

    Update

--- index.py
+++ index.py
@@ -12,18 +12,45 @@ def evaluate_calculation():
     try:
         calculation = str(eval(calculation))
         text_result.delete(1.0, "end")
-        text_result.
+        text_result.insert(1.0, calculation)
 
     except: 
-        pass
+        clear_field()
+        text_result.insert(1.0, "Error")
 
 def clear_field():
-    pass
+    global calculation
+    calculation = ""
+    text_result.delete(1.0, "end")
 
 root = tk.Tk()
 root.geometry("300x275")
 
-text_result - tk.Text(root, height=2, width=16 font=("Arial", 24))
+text_result = tk.Text(root, height=2, width=16, font=("Arial", 24))
 text_result.grid(columnspan=5)
+
+btn_1 = tk.Button(root, text="1", command=lambda: add_to_calculation(1), width=5, font=("Arial", 14))
+btn_1.grid(row=2, column=1)
+
+btn_2 = tk.Button(root, text="2", command=lambda: add_to_calculation(2), width=5, font=("Arial", 14))
+btn_2.grid(row=2, column=2)
+
+btn_3 = tk.Button(root, text="3", command=lambda: add_to_calculation(3), width=5, font=("Arial", 14))
+btn_3.grid(row=2, column=3)
+
+btn_4 = tk.Button(root, text="4", command=lambda: add_to_calculation(4), width=5, font=("Arial", 14))
+btn_4.grid(row=2, column=4)
+
+btn_5 = tk.Button(root, text="5", command=lambda: add_to_calculation(5), width=5, font=("Arial", 14))
+btn_5.grid(row=2, column=5)
+
+# btn_1 = tk.Button(root, text="1", command=lambda: add_to_calculation(1), width=5, font=("Arial", 14))
+# btn_1.grid(row=2, column=1)
+
+# btn_1 = tk.Button(root, text="1", command=lambda: add_to_calculation(1), width=5, font=("Arial", 14))
+# btn_1.grid(row=2, column=1)
+
+# btn_1 = tk.Button(root, text="1", command=lambda: add_to_calculation(1), width=5, font=("Arial", 14))
+# btn_1.grid(row=2, column=1)
 
 root.mainloop()
