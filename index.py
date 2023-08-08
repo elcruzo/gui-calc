c6c99e2d2d57fae938fccdac5957071bfe4fa10a
elcruzo <ayomideadekoya266@gmail.com>
2023-08-08 14:59:44 -0400

    Update

--- index.py
+++ index.py
@@ -14,4 +14,7 @@ def clear_field():
 root = tk.Tk()
 root.geometry("300x275")
 
-text_result - tk.Text(root, height=2, width=16)
+text_result - tk.Text(root, height=2, width=16 font=("Arial", 24))
+text_result.grid(columnspan=5)
+
+root.mainloop()
